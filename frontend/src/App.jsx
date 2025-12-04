70d0bd1213c5102a3d87026f79f605fcb295587a
raghed1234 <raghed.touska.1234@gmail.com>
2025-12-04 17:14:59 +0200

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [user, setUser] = useState(null);
