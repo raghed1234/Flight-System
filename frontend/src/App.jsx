42c89a03cc8318c05e1e269c3e58697b859dfc87
raghed1234 <raghed.touska.1234@gmail.com>
2025-12-04 17:17:11 +0200

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
